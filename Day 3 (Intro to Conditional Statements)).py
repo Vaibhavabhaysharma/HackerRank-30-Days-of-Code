a69b5abfa3caddd5a7e00fd2a4808a921f7c5482
Vaibhav Sharma <vaibhavmcudc145@gmail.com>
2020-08-05 19:01:11 +0530

    Update and rename Day 3 (Intro to Conditional Statements)) to Day 3 (Intro to Conditional Statements)).py

---
+++ Day 3 (Intro to Conditional Statements)).py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+N = int(input())
+if(N%2!=0):
+    print("Weird")
+elif(N>2 and N<5):
+    print("Not Weird")
+elif(N>6 and N<20):
+    print("Weird")
+elif(N>20):
+    print("Not Weird")
+else:
+    print("Weird")    
+
