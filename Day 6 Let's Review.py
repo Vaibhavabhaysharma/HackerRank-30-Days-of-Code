30425c1873a743e10a7f3a3f4064ecec1f2a8ed9
Vaibhav Sharma <vaibhavmcudc145@gmail.com>
2020-08-06 23:39:19 +0530

    Update Day 6 Let's Review.py

--- Day 6 Let's Review.py
+++ Day 6 Let's Review.py
@@ -1,1 +1,12 @@
+t = int(raw_input())
+for _ in range(t):
+    line = raw_input()
+    first = ""
+    second = ""
 
+    for i, c in enumerate(line):
+        if (i & 1) == 0:
+            first += c
+        else:
+            second += c
+    print first, second
