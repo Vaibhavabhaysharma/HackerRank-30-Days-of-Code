0bc9e91281b831daeac2749d6b02b55e3ecfbbca
Vaibhav Sharma <vaibhavmcudc145@gmail.com>
2020-08-06 22:54:41 +0530

    Update Day 5 Loops .py

--- Day 5 Loops .py
+++ Day 5 Loops .py
@@ -1,1 +1,14 @@
+#!/bin/python
 
+import math
+import os
+import random
+import re
+import sys
+
+i=0
+result=0
+n = int(raw_input())
+for i in range(1,11):
+    result = n*i
+    print(str(n)+' x '+str(i)+' = '+str(result))
