a13b43b860a565498dd3c22a63e4c499bbbf6578
Vaibhav Sharma <vaibhavmcudc145@gmail.com>
2020-08-07 22:55:53 +0530

    Update Day 7 Arrays.py

--- Day 7 Arrays.py
+++ Day 7 Arrays.py
@@ -1,1 +1,14 @@
+#!/bin/python
 
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    arr = map(int, raw_input().rstrip().split())
+    print(" ".join(map(str, arr[::-1])))
