52c714a69e69a675c369bd84ffc9258ad9581c7b
Vaibhav Sharma <vaibhavmcudc145@gmail.com>
2020-08-14 20:58:32 +0530

    Update Day 8 Dictionaries and Maps.py

--- Day 8 Dictionaries and Maps.py
+++ Day 8 Dictionaries and Maps.py
@@ -1,1 +1,10 @@
-Will be updated soon!!
+import sys
+n = input()
+name_numbers = [raw_input().split() for _ in range(n)]
+phone_book = {k: v for k,v in name_numbers}
+ls = map(lambda x: x.strip(),sys.stdin.readlines())
+for name in ls:
+    if name in phone_book:
+        print('%s=%s' % (name, phone_book[name]))
+    else:
+        print('Not found')
