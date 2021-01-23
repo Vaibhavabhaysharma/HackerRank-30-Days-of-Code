53e3b44083c66c5d656fa62448f792379b9c9ba7
Vaibhav Sharma <vaibhavmcudc145@gmail.com>
2021-01-23 19:52:45 +0530

    Coded @ Hackerrank🔰

--- Day 00 (Hello World).py
+++ Day 00 (Hello World).py
@@ -1,4 +1,3 @@
-# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
 input_string = input()
 
 # Print a string literal saying "Hello, World." to stdout.
